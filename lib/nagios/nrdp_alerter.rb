31a78998788ccc23d70dc7746c76b56138013ebf
Patrick St. Jean <stjeanp@pat-st-jean.com>
2015-07-28 09:42:47 -0500

    Added initial submit_command. Renamed alert to check throughout.

--- lib/nagios/nrdp_alerter.rb
+++ lib/nagios/nrdp_alerter.rb
@@ -3,7 +3,7 @@ require 'uri'
 require 'nokogiri'
 
 module Nagios
-  class NrdpAlerter
+  class Nrdp
     attr_accessor :url
     attr_accessor :token
 
@@ -28,17 +28,17 @@ module Nagios
       end
     end
 
-    def send_alert(*args)
+    def send_check(*args)
       if args[0].is_a? Hash
-        the_alerts = [args[0]]
+        the_checks = [args[0]]
       else
-        the_alerts = args[0]
+        the_checks = args[0]
       end
-      the_alerts.each do |the_alert|
-        validate_alert(the_alert)
+      the_checks.each do |the_check|
+        validate_check(the_check)
       end
 
-      payload = build_xml(the_alerts)
+      payload = build_xml(the_checks)
 
       the_uri = URI.parse(@url)
       http = Net::HTTP.new(the_uri.host, the_uri.port)
@@ -49,7 +49,7 @@ module Nagios
       request = Net::HTTP::Post.new(the_uri.request_uri + "/")
       request.set_form_data( { "token" => token, "cmd" => "submitcheck", "XMLDATA" => payload } )
       request['Accept'] = 'text/*'
-      request['User-Agent'] = 'NrdpAlerter/1.0'
+      request['User-Agent'] = 'NrdpClient/1.0'
       response = http.request(request)
 
       if response.code != "200"
@@ -67,7 +67,7 @@ module Nagios
 
       count_agrees = false
       doc.xpath("//output").each do |output|
-        if output.content == "#{the_alerts.count.to_s} checks processed."
+        if output.content == "#{the_checks.count.to_s} checks processed."
           count_agrees = true
         end
       end
@@ -76,36 +76,71 @@ module Nagios
       end
       true
     end
-    alias_method :send_alerts, :send_alert
+    alias_method :send_checks, :send_check
+
+    def submit_command(the_command = "")
+      if !the_command || !the_command.is_a? String || the_command.empty?
+        raise ArgumentError, "Invalid command supplied!"
+      end
+
+      the_uri = URI.parse(@url)
+      http = Net::HTTP.new(the_uri.host, the_uri.port)
+      if the_uri.scheme == "https"
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+      end
+      request = Net::HTTP::Get.new(the_uri.request_uri + "/")
+      request.set_form_data( { "token" => token, "cmd" => "submitcmd", "command" => the_command } )
+      request['Accept'] = 'text/*'
+      request['User-Agent'] = 'NrdpClient/1.0'
+      response = http.request(request)
+
+      if response.code != "200"
+        raise RuntimeError, "Didn't get a 200 (" + response.code.to_s + ")"
+      end
+
+      doc = Nokogiri::XML(response.body)
+
+      status = doc.xpath("//status").first.content.to_i
+      message = doc.xpath("//message").first.content
+
+      if status != 0
+        raise RuntimeError, "Status isn't 0 (" + message + ")"
+      end
+      true
+    end
 
     private
 
-    def validate_alert(the_alert = {})
-      if [:hostname, :state, :output].any? { |key| !the_alert.has_key?(key) }
-        raise ArgumentError, "You must provide all alert details!"
+    def validate_check(the_check = {})
+      if !the_check.keys.all? { |key| [:hostname, :servicename, :state, :output].include? key }
+        raise ArgumentError, "Unknown parameters in check!"
       end
-      if !the_alert[:state].kind_of? Integer
-        raise ArgumentError, "Alert state must be an integer!"
+      if [:hostname, :state, :output].any? { |key| !the_check.has_key?(key) }
+        raise ArgumentError, "You must provide all check details!"
+      end
+      if !the_check[:state].kind_of? Integer
+        raise ArgumentError, "Check's state must be an integer!"
       end
     end
 
-    def build_xml(the_alerts = [])
-      if the_alerts.count < 1
-        raise ArgumentError, "You must send at least one alert!"
+    def build_xml(the_checks = [])
+      if the_checks.count < 1
+        raise ArgumentError, "You must send at least one check!"
       end
 
       the_xml = "<?xml version='1.0'?>\n"
       the_xml += "<checkresults>\n"
-      the_alerts.each do |alert|
+      the_checks.each do |check|
         the_xml += "  <checkresult type='"
-        the_xml += alert[:servicename] ? 'service' : 'host'
+        the_xml += check[:servicename] ? 'service' : 'host'
         the_xml += "'>\n"
-        the_xml += "    <hostname>" + alert[:hostname] + "</hostname>\n"
-        if alert[:servicename]
-          the_xml += "    <servicename>" + alert[:servicename] + "</servicename>\n"
+        the_xml += "    <hostname>" + check[:hostname] + "</hostname>\n"
+        if check[:servicename]
+          the_xml += "    <servicename>" + check[:servicename] + "</servicename>\n"
         end
-        the_xml += "    <state>" + alert[:state].to_s + "</state>\n"
-        the_xml += "    <output>" + alert[:output] + "</output>\n"
+        the_xml += "    <state>" + check[:state].to_s + "</state>\n"
+        the_xml += "    <output>" + check[:output] + "</output>\n"
         the_xml += "  </checkresult>\n"
       end
       the_xml += "</checkresults>\n"
