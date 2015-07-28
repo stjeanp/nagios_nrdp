8073b679726523f5aec040b7ae69a230e522a432
Patrick St. Jean <stjeanp@pat-st-jean.com>
2015-07-28 15:37:33 -0500

    Rubocop cleanups

--- examples/submit_check.rb
+++ examples/submit_check.rb
@@ -6,19 +6,19 @@ def get_user_input(prompt)
   gets.chomp
 end
 
-url = get_user_input("NRDP URL")
-token = get_user_input("Token")
-hostname = get_user_input("Hostname")
-state = get_user_input("State")
-output = get_user_input("Output")
+url = get_user_input('NRDP URL')
+token = get_user_input('Token')
+hostname = get_user_input('Hostname')
+state = get_user_input('State')
+output = get_user_input('Output')
 
 puts
 
-nrdp = Nagios::Nrdp.new(:url => url, :token => token)
-if nrdp.submit_check(:hostname => hostname, :state => state.to_i, :output => output)
-  puts "Check submitted successfully."
+nrdp = Nagios::Nrdp.new(url: url, token: token)
+if nrdp.submit_check(hostname: hostname, state: state.to_i, output: output)
+  puts 'Check submitted successfully.'
   exit 0
 else
-  puts "Failed to submit check!"
+  puts 'Failed to submit check!'
   exit 1
 end
--- examples/submit_command.rb
+++ examples/submit_command.rb
@@ -6,17 +6,17 @@ def get_user_input(prompt)
   gets.chomp
 end
 
-url = get_user_input("NRDP URL")
-token = get_user_input("Token")
-command = get_user_input("Command")
+url = get_user_input('NRDP URL')
+token = get_user_input('Token')
+command = get_user_input('Command')
 
 puts
 
-nrdp = Nagios::Nrdp.new(:url => url, :token => token)
+nrdp = Nagios::Nrdp.new(url: url, token: token)
 if nrdp.submit_command(command)
-  puts "Command submitted successfully."
+  puts 'Command submitted successfully.'
   exit 0
 else
-  puts "Failed to submit command!"
+  puts 'Failed to submit command!'
   exit 1
 end
