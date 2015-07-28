2fd2eb919a9dd92c3bd38a4c66e52206a5f8528e
Patrick St. Jean <stjeanp@pat-st-jean.com>
2015-07-28 12:55:33 -0500

    Added simple usage examples

---
+++ examples/submit_check.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+require 'nagios_nrdp'
+
+def get_user_input(prompt)
+  print "#{prompt}: "
+  gets.chomp
+end
+
+url = get_user_input("NRDP URL")
+token = get_user_input("Token")
+hostname = get_user_input("Hostname")
+state = get_user_input("State")
+output = get_user_input("Output")
+
+puts
+
+nrdp = Nagios::Nrdp.new(:url => url, :token => token)
+if nrdp.submit_check(:hostname => hostname, :state => state.to_i, :output => output)
+  puts "Check submitted successfully."
+  exit 0
+else
+  puts "Failed to submit check!"
+  exit 1
+end
---
+++ examples/submit_command.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+require 'nagios_nrdp'
+
+def get_user_input(prompt)
+  print "#{prompt}: "
+  gets.chomp
+end
+
+url = get_user_input("NRDP URL")
+token = get_user_input("Token")
+command = get_user_input("Command")
+
+puts
+
+nrdp = Nagios::Nrdp.new(:url => url, :token => token)
+if nrdp.submit_command(command)
+  puts "Command submitted successfully."
+  exit 0
+else
+  puts "Failed to submit command!"
+  exit 1
+end
